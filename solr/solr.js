d848713691029dfc393e95116427ad291bbf4f67
nosvalds <niko@devinit.org>
2021-11-10 09:47:53 +0000

    authentication to solr working

--- solr/solr.js
+++ solr/solr.js
@@ -2,25 +2,38 @@ const fetch = require('node-fetch');
 const config = require('../config/config');
 const { checkRespStatus } = require('../utils/utils');
 
-// config.SOLR_URL https://default-iati-prod-solrcloud.solr.iatistandard.org/solr/
-
-const makeQueryParamStr = (paramObject) => Object.keys(paramObject).reduce((acc, key) => `${acc  }&${key}=${paramObject[key]}`, '');
+const makeQueryParamStr = (paramObject) =>
+    Object.keys(paramObject).reduce((acc, key) => `${acc}&${key}=${paramObject[key]}`, '');
 
 module.exports = {
     createCollection: async (name, colConfig) => {
         const queryParamString = makeQueryParamStr(colConfig);
         const response = await fetch(
-            `${config.SOLRCONFIG.url}admin/collections?action=CREATE&name=${name}${queryParamString}`
+            `${config.SOLRCONFIG.url}admin/collections?action=CREATE&name=${name}${queryParamString}`,
+            {
+                headers: {
+                    Authorization: `Basic ${Buffer.from(
+                        `${config.SOLRCONFIG.user}:${config.SOLRCONFIG.password}`,
+                        'binary'
+                    ).toString('base64')}`,
+                },
+            }
         );
         checkRespStatus(response);
-        
     },
 
     createOrUpdateAlias: async (aliasName, collectionName) => {
         const response = await fetch(
-            `${config.SOLRCONFIG.url}admin/collections?action=CREATEALIAS&name=${aliasName}&collections=${collectionName}`
+            `${config.SOLRCONFIG.url}admin/collections?action=CREATEALIAS&name=${aliasName}&collections=${collectionName}`,
+            {
+                headers: {
+                    Authorization: `Basic ${Buffer.from(
+                        `${config.SOLRCONFIG.user}:${config.SOLRCONFIG.password}`,
+                        'binary'
+                    ).toString('base64')}`,
+                },
+            }
         );
         checkRespStatus(response);
-        
     },
 };
